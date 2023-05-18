3ed306102759d7bd1093b62089365fad297fda22
Franzdomingo <106358025+Franzdomingo@users.noreply.github.com>
2023-05-18 14:31:36 +0800

    sir code

--- may18.php
+++ may18.php
@@ -1,28 +1,37 @@
-<?php
-class Fruit {
-  // Properties
-  public $name;
-  public $color;
+<!DOCTYPE html>
 
-  // Methods
-  function set_name($name) {
-    $this->name = $name;
-  }
-  function get_name() {
-    return $this->name;
-  }
-  function set_color($color) {
-    $this->color = $color;
-  }
-  function get_color() {
-    return $this->color;
-  }
-}
+<HTML>
+    <BODY>
+        <?php
+       
+        //Predefined Functions: Array
+            $colors = array("Red", "Green", "Blue");
 
-$apple = new Fruit();
-$apple->set_name('Apple');
-$apple->set_color('Red');
-echo "Name: " . $apple->get_name();
-echo "<br>";
-echo "Color: " . $apple->get_color();
-?>
+            echo "<br>Array Size: ".count($colors);
+
+            array_push($colors, "Black", "White");
+
+            echo "<br>Array Size: ".count($colors);
+
+            array_pop($colors);
+           
+       
+            //$reverse_array = array_reverse($colors);
+            var_dump($colors);
+
+            echo "<br>Colors: ";
+            foreach(array_reverse($colors) as $color){
+                echo "$color, ";
+            }
+
+            $grades = array(80,90.5,"100", "One");
+           
+            echo "<br>Grades:";
+            rsort($grades);
+            var_dump($grades);
+            echo "<br>Summation of Grades:".array_sum($grades);
+            echo "<br> Average: ".(array_sum($grades)/count($grades));
+
+        ?>
+    </BODY>
+</HTML>
